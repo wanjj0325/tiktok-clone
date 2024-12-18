aab0f4d4a0e331c5c47ecd3d9732da14da88ea5b
wanjj0325 <wanjunjie3@hotmail.com>
2024-12-18 09:10:43 +0800

    upload sidebar

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -36,13 +36,34 @@
                     <Icon class="ml-1 mr-4" name="carbon:send-alt" color="#161724" size="30"/>
                     <Icon class="mr-5" name="bx:message-detail" color="#161724" size="27"/>
                     <div class="relative">
-                        <button class="mt-1">
+                        <button class="mt-1" @click="showMenu = !showMenu">
                             <img 
                                 class="rounded-full" 
                                 width="33" 
                                 src="https://picsum.photos/id/83/300/320"
                             >
                         </button>
+                        <div 
+                            v-if="showMenu" 
+                            id="PopupMenu"
+                            class="absolute bg-white rounded-lg py-1.5 w-[200px] shadow-xl border top-[43px] -right-2"
+                        >
+                            <NuxtLink 
+                                
+                                @click="showMenu = false"
+                                class="flex items-center justify-start py-3 px-2 hover:bg-gray-100 cursor-pointer"
+                            >
+                                <Icon name="ph:user" size="20"/>
+                                <span class="pl-2 font-semibold text-sm">Profile</span>
+                            </NuxtLink>
+                            <div 
+                                
+                                class="flex items-center justify-start py-3 px-1.5 hover:bg-gray-100 border-t cursor-pointer"
+                            >
+                                <Icon name="ic:outline-login" size="20"/>
+                                <span class="pl-2 font-semibold text-sm">Log out</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -53,6 +74,7 @@
 
 <script setup>
     const route = useRoute()
+    let showMenu = ref(false)
 </script>
 
 <style lang="scss" scoped>
